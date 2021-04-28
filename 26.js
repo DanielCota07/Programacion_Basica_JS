64cab322cba3bc6b771196310e8a50312287d18a
Daniel Cota <danielalbertocotaochoa6@gmail.com>
2021-04-27 22:00:42 -0600

    Se agregaron mas problemas resueltos 2

--- 26.js
+++ 26.js
@@ -2,3 +2,5 @@ const prompt = require('prompt-sync')({sigint: true});
 
 let n = prompt('Ingrese su frase: ');
 n = String(n);
+var center = require('center-align');
+console.log(center(n, 145));
---
+++ 32.js
@@ -0,0 +1,8 @@
+var nombres = ["Daniel", "Dafne","Dali","Juan","Hugo","Wong",
+               "Maria","Chuy","Pepe", "Gilberto", "Joel","Fernando",
+               "Daniela", "Yaya", "Karla", "Denisse", "Ingrid", "Kenia",
+               "Ixchel", "Luis"];
+for(var i=0;i<nombres.length;i++){
+
+    console.log(nombres[i]);
+}
